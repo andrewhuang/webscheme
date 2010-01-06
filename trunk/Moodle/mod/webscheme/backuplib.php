a31dd21acde689ed4359ab3064eb6fc4688a3cb7
natetitterton <natetitterton@6fa9b66c-5620-0410-921c-6dc31a61c193>
2010-01-06 19:21:59 +0000

    uptodate - newmodule 1.9, backup stuff.  Testing to start now.

---
+++ trunk/Moodle/mod/webscheme/backuplib.php
@@ -0,0 +1,94 @@
+<?php
+
+    function webscheme_backup_mods($bf,$preferences) {
+
+        global $CFG, $DB;
+
+        $status = true;
+
+        //Iterate over choice table
+        $webschemes = $DB->get_records("webscheme", array("course" => $preferences->backup_course), "id");
+        if ($webschemes) {
+            foreach ($webschemes as $ws) {
+                if (backup_mod_selected($preferences,'webscheme',$choice->id)) {
+                    $status = webscheme_backup_one_mod($bf,$preferences,$ws);
+                }
+            }
+        }
+        // really?  return the status of the last one?  this is borrowed from choice...
+        return $status;
+    }
+    
+    
+   function webscheme_backup_one_mod($bf,$preferences,$ws) {
+
+        global $CFG, $DB;
+
+        if (is_numeric($ws)) {
+            $ws = $DB->get_record('webscheme',array('id' => $ws));
+        }
+
+        $status = true;
+
+        //Start mod
+        fwrite ($bf,start_tag("MOD",3,true));
+        //Print choice data
+        fwrite ($bf,full_tag("ID",4,false,$choice->id));
+        fwrite ($bf,full_tag("MODTYPE",4,false,"webscheme"));
+        fwrite ($bf,full_tag("NAME",4,false,$ws->name));
+        fwrite ($bf,full_tag("INTRO",4,false,$ws->intro));
+        fwrite ($bf,full_tag("INTROFORMAT",4,false,$ws->introformat));
+        fwrite ($bf,full_tag("TIMECREATED",4,false,$ws->timeccreated));
+        fwrite ($bf,full_tag("TIMEMODIFIED",4,false,$ws->timemodified));
+
+        $wsml = htmlentities($ws->wsml);  //so, html has be encoded twice now...
+        fwrite ($bf, full_tag("WSML",4,false,$wsml));
+
+        //End mod
+        $status = fwrite ($bf,end_tag("MOD",3,true));
+
+        return $status;
+      }
+      
+      
+     ////?? no real idea what this is doing. Copied mostly from mod/label
+     ////Return an array of info (name,value)
+     function webscheme_check_backup_mods($course,$user_data=false,$backup_unique_code,$instances=null) {
+        global $DB;
+
+        if (!empty($instances) && is_array($instances) && count($instances)) {
+            $info = array();
+            foreach ($instances as $id => $instance) {
+                $info += webscheme_check_backup_mods_instances($instance,$backup_unique_code);
+            }
+            return $info;
+        }
+
+         //First the course data
+         $info[0][0] = get_string("modulenameplural","label");
+         $info[0][1] = $DB->count_records("webscheme", array("course"=>$course));
+         return $info;
+         
+         //No user data, so nothing more...
+    }
+    
+    
+    ////?? no real idea what this is doing. Copied mostly from mod/label
+    ////Return an array of info (name,value)
+    function webscheme_check_backup_mods_instances($instance,$backup_unique_code) {
+         //First the course data
+        $info[$instance->id.'0'][0] = '<b>'.$instance->name.'</b>';
+        $info[$instance->id.'0'][1] = '';
+        return $info;
+    }
+    
+    
+    // no real idea here.  Supposed to munge $content links somehow? 
+    //   (it is in mod/choice)
+    function webscheme_encode_content_links($content,$preferences) {
+
+        global $CFG;
+        
+        return $content;  // un-munged
+    }
+?>
