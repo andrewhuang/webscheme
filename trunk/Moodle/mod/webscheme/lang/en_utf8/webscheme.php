613c4127fba79e4d837f3c073cc8a18d6a301426
natetitterton <natetitterton@6fa9b66c-5620-0410-921c-6dc31a61c193>
2010-01-08 21:27:22 +0000

    still failing on help, sigh

--- trunk/Moodle/mod/webscheme/lang/en_utf8/webscheme.php
+++ trunk/Moodle/mod/webscheme/lang/en_utf8/webscheme.php
@@ -8,24 +8,25 @@ $string['modulenameplural'] = 'WebSchemes';
 $string['webschemeintro'] = 'WebScheme Intro';
 $string['webschemename'] = 'WebScheme Name';
 
-$string['pagedefs'] = 'Page Definitions';
-$string['loadurls'] = 'Load URLs';
+
+$string['pagedefsheader'] = 'Page Definitions';
+$string['loadurls'] = 'Load URLs (whitespace separated)';
 $string['loadurlsdefault'] = "http://inst.eecs.berkeley.edu/~cs3/programs/webscheme-init-full.scm";
 
-$string['initexprs'] = 'Initial s-expressions';
+$string['initexpr'] = 'Initializing s-expression';
 $string['event'] = 'Event';
 $string['eventname'] = 'Name';
 $string['eventasserts'] = 'Assertions';
 $string['eventtemplate'] = 'Template';
 
 
-$string['pagecontent'] = 'Page Contents (HTML)';
+$string['content_header'] = 'Page Contents (HTML)';
 $string['contentmain'] = 'HTML with webscheme expressions';
 $string['contenthelp'] = 'Help with content';
 
 $string['webschemewsml'] = 'wsml entry';
 $string['wsmlParams'] = 'WSML';
-$string['wsmlHtml'] = 'HTML';
+$string['ws_html'] = 'HTML with web';
 
 $string['loadurldefault'] = "http://inst.eecs.berkeley.edu/~cs3/programs/webscheme-init-full.scm";
 ?>
