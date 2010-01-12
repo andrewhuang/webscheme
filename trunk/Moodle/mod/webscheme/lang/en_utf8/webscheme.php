85f9c15d08d5f8755f00b02cef1559b4a68d1496
natetitterton <natetitterton@6fa9b66c-5620-0410-921c-6dc31a61c193>
2010-01-12 21:46:51 +0000

    view.php in progress still, modform done

--- trunk/Moodle/mod/webscheme/lang/en_utf8/webscheme.php
+++ trunk/Moodle/mod/webscheme/lang/en_utf8/webscheme.php
@@ -12,21 +12,22 @@ $string['webschemename'] = 'WebScheme Name';
 $string['pagedefsheader'] = 'Page Definitions';
 $string['loadurls'] = 'Load URLs (whitespace separated)';
 $string['loadurlsdefault'] = "http://inst.eecs.berkeley.edu/~cs3/programs/webscheme-init-full.scm";
-
 $string['initexpr'] = 'Initializing s-expression';
-$string['event'] = 'Event';
+
+$string['event_header'] = 'Events';
 $string['eventname'] = 'Name';
 $string['eventasserts'] = 'Assertions';
 $string['eventtemplate'] = 'Template';
-
+$string['event_add_fields_button'] = 'Add {no} more blank event forms';
 
 $string['content_header'] = 'Page Contents (HTML)';
-$string['contentmain'] = 'HTML with webscheme expressions';
-$string['contenthelp'] = 'Help with content';
-
-$string['webschemewsml'] = 'wsml entry';
-$string['wsmlParams'] = 'WSML';
-$string['ws_html'] = 'HTML with web';
+$string['ws_html'] = 'HTML with webscheme expressions';
+$string['ws_html_help'] = 'Help with HTML';
 
 $string['loadurldefault'] = "http://inst.eecs.berkeley.edu/~cs3/programs/webscheme-init-full.scm";
+
+
+$string['badjsondecode_auth'] = 'Uh oh, the json didn\'t decode right here.  Badness.  Report this please.';
+
+$string['badjsondecode']='The definition of this activity has corrupted somehow; please report this problem.';
 ?>
--- trunk/Moodle/mod/webscheme/locallib.php
+++ trunk/Moodle/mod/webscheme/locallib.php
@@ -1,5 +1,5 @@
 <?php
-require_once("$CFG->dirroot/mod/newmodule/lib.php");
+require_once("$CFG->dirroot/mod/webscheme/lib.php");
 
 //////////////////////////////////////////////////////////////////////////////////////
 /// Any other webscheme functions go here.  Each of them must have a name that
